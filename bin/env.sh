421cb3221f83a69f7d38684e6c2158fb9569914e
Hakan Ilter <hakanilter@gmail.com>
2017-11-14 00:16:32 +0000

    fixed bash scripts

--- bin/env.sh
+++ bin/env.sh
@@ -2,7 +2,7 @@
 
 # configuration
 export SPARK_MASTER="local[*]"
-export ELASTIC_HOST="localhost"
+export ELASTIC_NODES="localhost"
 export ELASTIC_PORT="9200"
 export LIB_PATH=../target/*-dist.jar
 export OPTIONS="-Dspark.sql.warehouse.dir=/tmp/spark-warehouse"
